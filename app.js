632c5a1af3af5e72635a5b19d2eb3cf4655dacb9
TimofeyAdmiralov <168144795+TimofeyAdmiralov@users.noreply.github.com>
2025-06-07 03:25:47 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let p = document.createElement('b');
 let usercard = document.getElementById('usercard');
 
 // Используем шаблонную строку для вставки значений
-p.innerText = `Добро пожаловать, ${tg.initDataUnsafe?.user?.first_name || ''} ${tg.initDataUnsafe?.user?.last_name || ''}`;
+p.innerText = ```
+Добро пожаловать, 
+${tg.initDataUnsafe?.user?.first_name || ''} 
+${tg.initDataUnsafe?.user?.last_name || ''}```;
 
 usercard.appendChild(p);
